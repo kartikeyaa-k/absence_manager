97d15c50f9a38c859fcdfa2b9fa586daafdda140
Kartikeya Badekar <kartikeya.199231@gmail.com>
2025-04-20 16:16:09 +0400

    chore: define clean architecture folder structure

---
+++ lib/barrel.dart
@@ -0,0 +1,6 @@
+export 'src/domain/entity/absence.dart';
+export 'src/domain/repository/absence_repository.dart';
+export 'src/domain/usecase/get_absences.dart';
+export 'src/data/repository/absence_repository_impl.dart';
+export 'src/presentation/cubit/absence_cubit.dart';
+export 'src/presentation/page/absence_list_page.dart';
